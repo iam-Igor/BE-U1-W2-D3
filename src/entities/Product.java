4a44ad4104ddfe410734e8577f9dff714caa678e
Ygor Garofalo <igorgarofalo@gmail.com>
2023-12-06 13:38:19 +0100

    Initial commit

---
+++ src/entities/Product.java
@@ -0,0 +1,47 @@
+package entities;
+
+public class Product {
+
+    public long id;
+
+    public String name;
+
+    public String category;
+    public double price;
+
+    public Product(long id, String name, String category, double price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
